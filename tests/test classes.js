d7f30134d47248c5dcbfa22a45f2011b7b951ea2
Michael Enion <caewok_2000@yahoo.com>
2021-09-28 14:56:27 -0700

    Add tests for CCW and intersections

--- tests/test classes.js
+++ tests/test classes.js
@@ -4,8 +4,9 @@ GeomVector = game.modules.get("libgeometry").api.GeomVector;
 GeomPixelVector = game.modules.get("libgeometry").api.GeomPixelVector;
 GeomLine = game.modules.get("libgeometry").api.GeomLine;
 GeomPixelLine = game.modules.get("libgeometry").api.GeomPixelLine;
-GeomPixelLine = game.modules.get("libgeometry").api.clearDrawings;
+clearDrawings = game.modules.get("libgeometry").api.clearDrawings;
 COLORS = game.modules.get("libgeometry").api.COLORS;
+GEOM_CONSTANTS = game.modules.get("libgeometry").api.GEOM_CONSTANTS;
 
 function drawSegment(p0, p1, color = COLORS.blue, alpha = 1, width = 1) {
     canvas.controls.debug.lineStyle(width, color, alpha).
@@ -76,10 +77,128 @@ l2.draw(COLORS.orange)
 // Test CCW -------------------------------
 
 // Vector
+clearDrawings()
+origin = new GeomPoint(1000, 1000, 0);
+p_q1 = new GeomPoint(500, 500, 0)
+p_q2 = new GeomPoint(1300, 700, 0)
+p_q3 = new GeomPoint(1300, 1200, 0)
+p_q4 = new GeomPoint(700, 1200, 0)
+
+p_up = new GeomPoint(1000, 500, 0)
+p_down = new GeomPoint(1000, 1500, 0)
+p_left = new GeomPoint(500, 1000, 0)
+p_right = new GeomPoint(1500, 1000, 0)
+
+
+v_up = new GeomVector(0, -1000, 0);
+v_left = new GeomVector(-500, 0, 0);
+
+// angled
+
+v_up.draw(origin, COLORS.blue);
+p_q1.draw(COLORS.red);
+
+assert(v_up.ccw2D(p_q1.subtract(origin), "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE, 
+       "Point Q1 CCW");
+       
+p_q2.draw(COLORS.red);       
+assert(v_up.ccw2D(p_q2.subtract(origin), "XY") === GEOM_CONSTANTS.CLOCKWISE, 
+       "Point Q2 CCW");
+
+p_q3.draw(COLORS.red);
+assert(v_up.ccw2D(p_q3.subtract(origin), "XY") === GEOM_CONSTANTS.CLOCKWISE, 
+       "Point Q3 CCW");
+
+p_q4.draw(COLORS.red);
+assert(v_up.ccw2D(p_q4.subtract(origin), "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE, 
+       "Point Q4 CCW");
+
+// coincident
+assert(v_up.ccw2D(p_up.subtract(origin), "XY") === GEOM_CONSTANTS.COLLINEAR,
+       "Point Up CCW");
+
+assert(v_up.ccw2D(p_down.subtract(origin), "XY") === GEOM_CONSTANTS.COLLINEAR,
+       "Point Down CCW");
+              
+// perpendicular
+v_left.draw(origin, COLORS.lightblue);
+assert(v_left.ccw2D(v_up, "XY") === GEOM_CONSTANTS.CLOCKWISE,
+       "Vector left CCW");
+       
+assert(v_up.ccw2D(v_left, "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE,
+       "Vector left CCW");
+
+assert(v_up.ccw2D(p_left.subtract(origin), "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE,
+       "Point left CCW")
 
 // Line
+clearDrawings()
+l_horizontal = new GeomLine(origin, v_left)
+l_vertical = new GeomLine(origin, v_up)
+l45 = new GeomLine(origin, new GeomVector(100, 100, 0));
+l120 = new GeomLine(origin, new GeomVector(200, -100, 0));
 
+l_horizontal.draw(COLORS.blue)
+l_vertical.draw(COLORS.blue)
+l45.draw(COLORS.lightblue)
+l120.draw(COLORS.green)
+
+// angles
+assert(l_vertical.ccw2D(p_q1, "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE, 
+       "Line Vertical Q1 CCW");
+assert(l_vertical.ccw2D(p_q2, "XY") === GEOM_CONSTANTS.CLOCKWISE, 
+       "Line Vertical Q2 CCW");
+assert(l_vertical.ccw2D(p_q3, "XY") === GEOM_CONSTANTS.CLOCKWISE, 
+       "Line Vertical Q3 CCW");
+assert(l_vertical.ccw2D(p_q4, "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE, 
+       "Line Vertical Q4 CCW");
+
+// perpendicular
+assert(l_vertical.ccw2D(p_left, "XY") === GEOM_CONSTANTS.COUNTERCLOCKWISE,
+       "Line Vertical Left CCW");
+       
+// coincident
+assert(l_vertical.ccw2D(p_up, "XY") === GEOM_CONSTANTS.COLLINEAR,
+       "Line Vertical Up CCW");
+
+assert(l_vertical.ccw2D(p_down, "XY") === GEOM_CONSTANTS.COLLINEAR,
+       "Line Vertical Down CCW");
 
 // Test intersections
+
+// parallel
+l45_parallel = new GeomLine({x: origin.x + 100, y: origin.y + 10, z: origin.z}, l45.v);
+l45_parallel.draw(COLORS.blue);
+
+assert(!l45.intersects2D(l45_parallel, "XY"), "Line 45 Parallel Intersects");
+assert(l45.parallel2D(l45_parallel, "XY", "Line 45 Parallel"));
+
+// perpendicular
+assert(l_horizontal.perpendicular2D(l_vertical, "XY"), "Line Horizontal/Vertical Perpendicular");
+
+// angles
+assert(l_horizontal.intersects2D(l45, "XY"), "Line Horizontal/45 Intersect")
+assert(l120.intersects2D(l45, "XY"), "Line 120/45 Intersect")
+
+// actual intersection points
+assert(l_horizontal.intersection2D(l45, "XY") === origin, "Line Horizontal/45 Intersection")
+
+l_horizontal.intersection2D(l45, "XY", as_point = false)
+
+// 3-D intersection
+l45 = new GeomLine(origin, new GeomVector(100, 100, 100));
+l135 = new GeomLine(origin, new GeomVector(100, -100, 100));
+
+assert(l45.intersects(l135), "Line 45/135 3D Intersect");
+
+// 3-D parallel
+l45_parallel = new GeomLine({x: origin.x + 100, y: origin.y + 10, z: origin.z + 20}, l45.v);
+assert(l45.parallel(l45_parallel), "Line 45 3D Parallel");
+
+// 3-D orthogonal
+assert(l45.perpendicular(l135), "Line 45/135 3D Perpendicular");
+assert(l_horizontal.perpendicular(l_vertical), 
+       "Line Vertical/Horizontal 3D Perpendicular");
+
 
 
