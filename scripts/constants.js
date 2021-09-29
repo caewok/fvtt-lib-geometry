268fbb9288a47bc6c79f4889ab4dc28a17d04317
Michael Enion <caewok_2000@yahoo.com>
2021-09-29 03:40:16 -0700

    Organize colors

--- scripts/constants.js
+++ scripts/constants.js
@@ -8,7 +8,7 @@ export const GEOM = {
   COPLANER: 0,
   DOWN: -1,
   LEFT: 1,
-  RIGHT: -1
+  RIGHT: -1,
   XY: 1,
   YZ: 2,
   XZ: 3
@@ -19,14 +19,37 @@ export const GEOM = {
  * @constant 
  */
 export const COLORS = {
-  orange: 0xFFA500,
+  // primary
+  red: 0xFF0000,
+  lightred: 0xFF7F7F,
+  darkred: 0x8B0000,
+  
   yellow: 0xFFFF00,
-  green: 0x00FF00, 
-  greenyellow: 0xADFF2F,
+  lightyellow: 0xFFFFED,
+  darkyellow: 0x8B8000,
+
   blue: 0x0000FF,
   lightblue: 0xADD8E6,
-  red: 0xFF0000,
+  darkblue: 0x00008B,
+  
+  // secondary
+  orange: 0xFFA500,
+  lightorange: 0xFFD580,
+  darkorange: 0xFF8C00,
+  
+  green: 0x00FF00, 
+  lightgreen: 0x90EE90,
+  darkgreen: 0x006400,
+  
+  violet: 0x8F00FF,
+  lightviolet: 0xCF9FFF,
+  darkviolet: 0x9400D3,
+  
+  // achromatic
   gray: 0x808080,
+  lightgray: 0xD3D3D3,
+  darkgray: 0xA9A9A9,
+  
   black: 0x000000,
   white: 0xFFFFFF
 }
