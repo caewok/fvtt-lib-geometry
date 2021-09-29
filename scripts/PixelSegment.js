29dc9477cee1a87544a3abdfd711d4ec958512b4
Michael Enion <caewok_2000@yahoo.com>
2021-09-29 16:10:40 -0700

    Add PixelSegment class

---
+++ scripts/PixelSegment.js
@@ -0,0 +1,30 @@
+import { GeomRay } from "./Line.js";
+
+
+/**
+ * An line from one point to a second point
+ * Represented by the parametric form of the equation for a line,
+ *   p + t*v
+ * t is constrained to be only between 0 and 1, where 0 gives p and 1 gives the endpoint.
+ * v magnitude gives the end point.
+ * This version uses GeomPixel classes for p and v.
+ * @param {GeomPixelPoint}  p  Origination point for the ray.
+ * @param {GeomPixelVector} v  Direction vector of the ray.
+ */ 
+export class GeomPixelSegment extends GeomSegment {
+  constructor(p, v) {
+    if(!(p instanceof GeomPixelPoint)) {
+      p = new GeomPixelPoint(p.x, p.y, p.z);
+    }
+    
+    if(!(v instanceof GeomPixelVector)) {
+      v = new GeomPixelVector(p.x, p.y, p.z);
+    }
+  
+    super(p, v);  
+  }
+
+  // -------------- METHODS --------------------- // 
+
+
+}
